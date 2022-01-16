f4538ad5c0b7adf1829c1cdef0f42c91b7ed5e83
Sara Ćurak <73559621+SaraCurak@users.noreply.github.com>
2022-01-16 22:30:56 +0100

    Delete app_topnav.php

--- Desktop/IMS/pzi - projekt/partials/app_topnav.php
+++
@@ -1,4 +0,0 @@
-<div class="dashboard_topNav">
-            <a href="" id="menuButton"><i class="fa fa-navicon"></i></a>
-            <a href="database/logout.php" id="logoutButton"><i class="fa fa-power-off"></i>Odjavi se</a>
-</div>
